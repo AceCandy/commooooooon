1f357d2aa911109d8b06b57243fb5b918789b548
AceCandy <804649325@qq.com>
2018-03-22 19:03:20 +0800

    修改了一些小bug

--- commooooooon/src/main/java/com/br/common/util/BrExecutors.java
+++ commooooooon/src/main/java/com/br/common/util/BrExecutors.java
@@ -8,7 +8,6 @@ import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * <p>Description: Executors 的四种线程池不满足需求，自定义</p>
  * 
@@ -18,50 +17,49 @@ import org.slf4j.LoggerFactory;
  * @version: 1.0
  */
 public class BrExecutors {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(BrExecutors.class);
-	
-	public static ThreadPoolExecutor other = new ThreadPoolExecutor(50, 50,60L, TimeUnit.SECONDS,new ArrayBlockingQueue<Runnable>(50000),new CallerRunsPolicy2());
-	
+
+	public static ThreadPoolExecutor other = new ThreadPoolExecutor(50, 50, 60L, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(50000), new CallerRunsPolicy2());
+
 	/**
 	 * 默认大小、初始化线程 100 最大 500
 	 * @return
 	 */
-	public static ThreadPoolExecutor getThreadPool(){
+	public static ThreadPoolExecutor getThreadPool() {
 		return getThreadPool(100, 500);
 	}
-	
-	public static ThreadPoolExecutor getThreadPool(int initNum,int maxNum){
-//		return new ThreadPoolExecutor(initNum, maxNum,60L, TimeUnit.SECONDS,new SynchronousQueue<Runnable>(),new MyRejectedExecutionHandler());
-		//CallerRunsPolicy 策略 线程调用运行该任务的 execute 本身。此策略提供简单的反馈控制机制，能够减缓新任务的提交速度
-		return new ThreadPoolExecutor(initNum, maxNum,60L, TimeUnit.SECONDS,new ArrayBlockingQueue<Runnable>(1200),new ThreadPoolExecutor.CallerRunsPolicy());
+
+	public static ThreadPoolExecutor getThreadPool(int initNum, int maxNum) {
+		// return new ThreadPoolExecutor(initNum, maxNum,60L, TimeUnit.SECONDS,new SynchronousQueue<Runnable>(),new
+		// MyRejectedExecutionHandler());
+		// CallerRunsPolicy 策略 线程调用运行该任务的 execute 本身。此策略提供简单的反馈控制机制，能够减缓新任务的提交速度
+		return new ThreadPoolExecutor(initNum, maxNum, 60L, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(1200), new ThreadPoolExecutor.CallerRunsPolicy());
 	}
-	
-	
+
 	public static class MyRejectedExecutionHandler implements RejectedExecutionHandler {
 
 		@Override
 		public void rejectedExecution(Runnable task, ThreadPoolExecutor executor) {
-//			new Thread(task).start();
-//			if (!executor.isShutdown()) {
-//				task.run();
-//            }
+			// new Thread(task).start();
+			// if (!executor.isShutdown()) {
+			// task.run();
+			// }
 			other.execute(task);
-			logger.warn("线程池已经达到边界值，将由other线程池执行，建议加大线程池，{}",executor.toString());
-			//打印线程池的对象
-//			System.out.println("The pool RejectedExecutionHandler = "+other.toString());
+			logger.warn("线程池已经达到边界值，将由other线程池执行，建议加大线程池，{}", executor.toString());
+			// 打印线程池的对象
+			// System.out.println("The pool RejectedExecutionHandler = "+other.toString());
 		}
 	}
-	
+
 	public static class CallerRunsPolicy2 implements RejectedExecutionHandler {
-        public CallerRunsPolicy2() { }
+		public CallerRunsPolicy2() {
+		}
 
-        public void rejectedExecution(Runnable r, ThreadPoolExecutor e) {
-            if (!e.isShutdown()) {
-                r.run();
-            }
-        }
-    }
+		public void rejectedExecution(Runnable r, ThreadPoolExecutor e) {
+			if (!e.isShutdown()) {
+				r.run();
+			}
+		}
+	}
 }
-
-
